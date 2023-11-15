ed2ec71ae1eb3ecc0e88e0e65c7579b9a5c7a9f4
gdidmanidze <gdidmanidze@ucvts.org>
2023-11-15 12:25:15 -0500

    Workout Tracker Base

--- app/src/main/java/com/example/fbla/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/fbla/ui/home/HomeFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.fbla.R
 import com.example.fbla.databinding.FragmentHomeBinding
+
+
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -28,8 +32,22 @@ class HomeFragment : Fragment() {
             textView.text = text
         })
 
+
+        val myButton: Button = findViewById(R.id.myButton)
+
+        myButton.setOnClickListener {
+            print("hello world")
+         }
+
+
         return root
     }
+
+    private fun findViewById(myButton: Int): Button {
+
+        return TODO("Provide the return value")
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
