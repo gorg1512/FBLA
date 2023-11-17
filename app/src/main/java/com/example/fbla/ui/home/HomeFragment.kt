181382ebc9f8a9759204440f1b06d196f9115a0b
Jake <jleyzerovich@ucvts.org>
2023-11-17 13:05:21 -0500

    Nov 17 Edits

--- app/src/main/java/com/example/fbla/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/fbla/ui/home/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.example.fbla.ui.home
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.fbla.R
 import com.example.fbla.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -21,12 +24,10 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-
         homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
-
         binding.btnAddWorkoutLinearLayout.setOnClickListener {
-            addNewWorkout()
+            showAddWorkoutDialog()
         }
 
         homeViewModel.workouts.observe(viewLifecycleOwner) { workouts ->
@@ -36,23 +37,40 @@ class HomeFragment : Fragment() {
         return root
     }
 
-    private fun displayWorkouts(workouts: List<Workout>) {
+    private fun showAddWorkoutDialog() {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_add_workout, null)
+        val editTextName = dialogView.findViewById<EditText>(R.id.editTextWorkoutName)
+        val editTextSets = dialogView.findViewById<EditText>(R.id.editTextSets)
+        val editTextReps = dialogView.findViewById<EditText>(R.id.editTextReps)
 
+        // Create a dialog
+        val dialog = AlertDialog.Builder(requireContext())
+            .setTitle("Add New Workout")
+            .setView(dialogView)
+            .setPositiveButton("Add") { _, _ ->
+                // Get user input and add a new workout
+                val name = editTextName.text.toString()
+                val sets = editTextSets.text.toString().toIntOrNull() ?: 0
+                val reps = editTextReps.text.toString().toIntOrNull() ?: 0
+                val newWorkout = Workout(name, sets, reps, "Description") // Example values, replace with actual data
+                homeViewModel.addWorkout(newWorkout)
+            }
+            .setNegativeButton("Cancel", null)
+            .create()
+
+        dialog.show()
+    }
+
+    private fun displayWorkouts(workouts: List<Workout>) {
         binding.workoutList.text = buildWorkoutDetails(workouts)
     }
 
     private fun buildWorkoutDetails(workouts: List<Workout>): String {
         val stringBuilder = StringBuilder()
         for (workout in workouts) {
-            stringBuilder.append("Workout: ${workout.name}, Duration: ${workout.durationMinutes} minutes\n\n\n")
-
+            stringBuilder.append("Workout: ${workout.name}, Sets: ${workout.sets}, Reps: ${workout.reps}\n\n\n")
         }
         return stringBuilder.toString()
-    }
-
-    private fun addNewWorkout() {
-        val newWorkout = Workout("New Workout", 30, "Description") // Example values, replace with actual data
-        homeViewModel.addWorkout(newWorkout)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/fbla/ui/home/Workout.kt
+++ app/src/main/java/com/example/fbla/ui/home/Workout.kt
@@ -1,7 +1,3 @@
 package com.example.fbla.ui.home
 
-data class Workout(
-    val name: String,
-    val durationMinutes: Int,
-    val description: String
-)
+data class Workout(val name: String, val sets: Int, val reps: Int, val description: String)
